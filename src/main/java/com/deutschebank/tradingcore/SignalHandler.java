2cd2b3e7cf03b89ea06a03cfe84a88ac140be23a
Atul Gupta <atul.gupta@kpsbremen.de>
2023-07-31 11:42:30 +0200

    chore: set up project with provided classes & interface Create class Algo, Application Create interface SignalHandler Configure gradle for code formatting, added format plugin Update .gitIgnore

---
+++ src/main/java/com/deutschebank/tradingcore/SignalHandler.java
@@ -0,0 +1,11 @@
+package com.deutschebank.tradingcore;
+
+/**
+ * This is a callback interface from our trading system, and we cannot change it. We want
+ * to replace the callback with an HTTP API.
+ */
+public interface SignalHandler {
+
+	void handleSignal(int signal);
+
+}
